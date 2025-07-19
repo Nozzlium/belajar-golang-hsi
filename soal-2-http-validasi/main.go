9c92f5456e6bbfa2c923f090e829c69c32438f83
Cesa Anwar <Cesa263@gmail.com>
2025-07-19 11:53:58 +0700

    git commit -m "fix(soal-2): perbaikan mengimplementasi library yang required"

--- soal-2-http-validasi/main.go
+++ soal-2-http-validasi/main.go
@@ -1,15 +1,78 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"net/http"
+	"strconv"
 
-	"github.com/nozzlium/belajar-golang-hsi/soal-2-http-validasi/controller"
-	"github.com/nozzlium/belajar-golang-hsi/soal-2-http-validasi/service"
+	log "github.com/sirupsen/logrus"
 )
 
+var ErrInvalidInput = errors.New("email kosong atau umur kurang dari 18")
+
+type AgeService struct {
+}
+
+func NewAgeService() *AgeService {
+	return &AgeService{}
+}
+
+func (service *AgeService) Validate(email string, age int) (string, error) {
+	if email != "" && age >= 18 {
+		return "ok", nil
+	}
+	log.WithFields(log.Fields{
+		"location": "AgeService.Validate",
+	}).Error(ErrInvalidInput.Error())
+	return "", fmt.Errorf("%w", ErrInvalidInput)
+}
+
+type AgeController struct {
+	AgeService *AgeService
+}
+
+func NewAgeController(ageService *AgeService) *AgeController {
+	return &AgeController{
+		AgeService: ageService,
+	}
+}
+
+func (controller *AgeController) Validate(w http.ResponseWriter, r *http.Request) {
+	type Response struct {
+		Status string `json:"status,omitempty"`
+		Error  string `json:"error,omitempty"`
+	}
+
+	if method := r.Method; method != "GET" {
+		http.Error(w, "invalid method", http.StatusMethodNotAllowed)
+	}
+
+	email := r.URL.Query().Get("email")
+	ageString := r.URL.Query().Get("age")
+	age, err := strconv.Atoi(ageString)
+	if err != nil {
+		http.Error(w, "invalid age format", http.StatusBadRequest)
+	}
+
+	encoder := json.NewEncoder(w)
+	w.Header().Set("Content-Type", "application/json")
+	status, err := controller.AgeService.Validate(email, age)
+	if err != nil && errors.Is(err, ErrInvalidInput) {
+		encoder.Encode(Response{
+			Error: "email kosong atau umur kurang dari 18",
+		})
+		return
+	}
+	encoder.Encode(Response{
+		Status: status,
+	})
+}
+
 func main() {
-	ageService := service.NewAgeService()
-	ageController := controller.NewAgeController(ageService)
+	ageService := NewAgeService()
+	ageController := NewAgeController(ageService)
 
 	http.HandleFunc("/validate", ageController.Validate)
 	http.ListenAndServe(":8080", nil)
